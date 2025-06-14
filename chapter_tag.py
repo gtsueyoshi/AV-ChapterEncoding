c27266cfae73a48f220f48b4f9ad8f1b3e8fc72d
Glenn Sueyoshi <gtsueyoshi@gmail.com>
2025-06-14 11:09:53 -0700

    Modify routines to use Python dictionaries for parsing structures

--- chapter_tag.py
+++ chapter_tag.py
@@ -1,8 +1,8 @@
 
 # read the process control file
-def parse_control_file(process_file):
+def parse_control_file(cmd_file, allowed_keys):
     """
-    parse the chapter tag control flie
+    parse the chapter tag command flie
 
     syntax for each line:
 
@@ -11,9 +11,9 @@ def parse_control_file(process_file):
     . [key] = [arg]
 
         where key = { "time", "source", "type", "output" } and
-            : time [arg] is the name of the timecode file with lines of the form "hh:mm:ss {chaptername}
+            : chapters [arg] is the name of the timecode file with lines of the form "hh:mm:ss {chaptername}
             : source [arg] is the original audio or video source file
-            : type [arg] is { "audio", "video" } for encoding
+            : srctype [arg] is { "audio", "video" } for encoding
             : output [arg] is the name of the base of the output flie
             : metaname [arg] is the optional name of the metadata temp file
             : batchname [arg] is the optional name of the ffmpet split batch file
@@ -21,43 +21,39 @@ def parse_control_file(process_file):
     . "split" is provided if you wish to split the output into individual chapter files
     . "keep" is provided if you wish to keep the metadata file and the batch file
 
-    . all other lines are ignored
-
     """
 
-    size = 8
-    input_data = [None] * size # [ timecode_file, source_file, source_type, output_file, split_bool, keep_temps]
-    process_file_nq = process_file.strip('"') # remove surrounding double quote because open doesn't like
+    # initialize the input data dictionary
+    input_data = {}
+
+    # remove surrounding quotes because open doesn't like
+    cmd_file_nq = cmd_file.strip('"').strip("'")
     try:
-         with open(process_file_nq, 'r') as file:
+         with open(cmd_file_nq, 'r') as file:
             for line in file:
-                # handle comment
-                if (line[0] == "#"):
+                 # remove leading and trailing whitespace
+                line = line.strip()
+
+                # skip empty or comment line
+                if (line == '' or line[0] == "#" or line[0:1] == "//"):
                     continue
-                # split the line by a delimiter (e.g., space, tab, etc.)
-                parts = line.strip().split()
-                key = parts[0].lower()
-                if (key == "split"):
-                    input_data[4] = True
-                elif (key == "keep"):
-                    input_data[5] = True
-                elif (parts[1] == "="):
-                    _text =  string_data = ' '.join(parts[2:]).strip() # Join the rest of the parts into a string
-                    if (key == "time"): # time code file
-                        input_data[0] = _text
-                    elif (key == "source"): # source file
-                        input_data[1] = _text
-                    elif (key == "type"): # video / audio type
-                        input_data[2] = _text
-                    elif (key == "output"): # output file
-                        input_data[3] = _text
-                    elif (key == "metafile"): # metadata file
-                        input_data[6] = _text
-                    elif (key == "batchfile"): # ffmpeg batch file
-                        input_data[7] = _text
+    
+                # process the line with '='
+                if '=' in line:
+                    key, value = line.split('=', 1)
+                    key = key.strip().lower()
+                    value = value.strip()
+                    if allowed_keys is None or key in allowed_keys:
+                        input_data[key] = value # dictionary style key=value pair
+                
+                # process the line without '='
+                else:
+                    line.strip().lower()
+                    if allowed_keys is None or line in allowed_keys:
+                        input_data[line] = True  # flag-style keyword
 
     except FileNotFoundError:
-        print(f"Error: File not found at {process_file}")
+        print(f"Error: File not found at {cmd_file}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -80,7 +76,7 @@ def read_timecode_data(timecode_file):
 
     time_data = []
     try:
-         timecode_file_nq = timecode_file.strip('"') # remove surrounding double quote because open doesn't like
+         timecode_file_nq = timecode_file.strip('"').strip("'") # remove surrounding quotes because open doesn't like
          with open(timecode_file_nq, 'r') as file:
             for line in file:
                 # Split the line by a delimiter (e.g., space, tab, etc.)
@@ -93,7 +89,7 @@ def read_timecode_data(timecode_file):
                     hours, minutes, seconds = map(int, time_code.split(':')) # split time code into h, m, s
 
                     string_data = ' '.join(parts[1:]) # Join the rest of the parts into a string
-                    time_data.append((time_code, string_data, hours, minutes, seconds))
+                    time_data.append((time_code, string_data, hours, minutes, seconds)) # add entry to list
 
     except FileNotFoundError:
         print(f"Error: File not found at {timecode_file}")
@@ -104,60 +100,90 @@ def read_timecode_data(timecode_file):
     return time_data
 
 
-
 # cut source file into chapters
-def process_chapters(chapter_data, process_input):
+def process_chapters(cmdfile):
     """
-    add chapter markers or cut video or audio source file into chapters using ffmpeg
+    wrapper to first parse the control file and information on allowed keys
+    and to create a parsed control dictionary, and then to process the chapter information 
+    using the parsed control dictionary and the allowed keys
 
     Args:
-        chapter_data: list of the starting time codes and the chapter names
-        process_input: the parsed input
+        cmdfile: the command file that contains the process command
 
     """
-    # analyze the chapter times file
-    chapter_path = process_input[0] # chapter information
-    chapter_data = read_timecode_data(chapter_path)
 
-    # create lag of chapter time lists
-    import itertools
-    chapter_data_lead_1 = itertools.islice(chapter_data, 1, None, 1)
+    # allowed keys in the control file
+    allowed_keys = ["chapters", "source", "srctype", "output", "split", "keep", "metaname", "batchname"]
 
-     # assign the basic information
-    src_file = process_input[1] # source file
-    src_type = process_input[2] # source type ("audio", "video")
-    out_file = process_input[3] # output base name
-    split_file = process_input[4] # flag for splitting into individual files
-    keep_flag = process_input[5] # flag for keeping the temporary flies
+    # parse the control file and return processing input dictionary
+    process_input = parse_control_file(cmdfile, allowed_keys)
+    control_values = [process_input.get(key, None) for key in allowed_keys]
 
-    # assign conversion files
-    meta_file = "ffmetadatfile.txt"
-    batch_file = "ffmpegbatch.bat"
-    if (process_input[6] != None): # metadata file name
-        meta_file = process_input[6]
-    if (process_input[7] != None): # batch file name
-        batch_file = process_input[7] 
+    # process the chapter information
+    do_chapters(control_values)
+
+
+# process the chapter information
+def do_chapters(control_values):
     
-    # no quote versions for open commands
-    meta_file_nq = meta_file.strip('"')
-    batch_file_nq = batch_file.strip('"')
+    """
+    add chapter markers or cut video or audio source file into chapters using ffmpeg
+    and the ordered list of control values
+
+    Args:
+        control_values: the ordered list of values from the process input dictionary
+ 
+    The control values are expected to be in the following order:
+
+    control_values = [ chapter_file, src_file, src_type, out_file, split_file, keep_flag, meta_file, batch_file ]
+
+    where:  
+        1. chapter_file: the name of the timecode file with lines of the form "hh:mm:ss {chaptername}"
+        2. src_file: the original audio or video source file
+        3. src_type: { "audio", "video" } for encoding
+        4. out_file: the name of the base of the output file
+        5. split_file: True if you wish to split the output into individual chapter files
+        6. keep_flag: True if you wish to keep the metadata file and the batch file
+        7. meta_file: optional name of the metadata temp file
+        8. batch_file: optional name of the ffmpeg split batch file
+
+    """
+        
+    # unpack the process input dictionary
+    if len(control_values) != 8:
+        print("Error: Invalid number of control values. Expected 8 values.")
+        return 
+    chapter_file, src_file, src_type, out_file, split_file, keep_flag, meta_file, batch_file = control_values
 
     # processing bools
     do_full = (out_file != None)
     do_audio = (src_type == "audio")
-                                   
+ 
+    # get chapter file name for open
+    chapter_file_nq = chapter_file.strip('"').strip("'") # remove surrounding quotes because open doesn't like
+                                  
+    # read the chapter data from the timecode file
+    chapter_data = read_timecode_data(chapter_file_nq)
+    if (len(chapter_data) == 0):
+        print(f"Error: No chapter data found in {chapter_file}")
+        return
+    
+    # create lag of chapter time lists
+    import itertools
+    chapter_data_lead_1 = itertools.islice(chapter_data, 1, None, 1)
+
     # get source file extension and directory
     import os
-    src_file_nq = src_file.strip('"') # remove surrounding double quote because os.path doesn't use
+    src_file_nq = src_file.strip('"').strip("'") # remove surrounding quotes because os.path doesn't use
     src_filename, src_extension = os.path.splitext(src_file_nq)
     src_dir = os.path.dirname(src_file_nq)
-    src_dir_nq = src_dir.strip('"')
+    src_dir_nq = src_dir.strip('"').strip("'") # remove surrounding quotes
  
     # contant strings
+    ffcmd = 'ffmpeg'
     chapterkey = '[CHAPTER]'
     timedef = 'TIMEBASE=1/1000'
     emptyline = ''
-    ffcmd = 'ffmpeg '
     ext = src_extension # original extension is the source file
     ffopt = ' -codec copy '
 
@@ -168,26 +194,33 @@ def process_chapters(chapter_data, process_input):
 
     # add or replace output extension
     if (do_full == True):
-        out_file_nq = out_file.strip('"') # remove surrounding double quote because os.path doesn't use
+        # update the output file name to have the correct extension
+        out_file_nq = out_file.strip('"') # remove surrounding quotes because os.path doesn't use
         out_filename, out_extension = os.path.splitext(out_file_nq)
-        out_file = '"' + out_filename + ext + '"' # add back surrounding double quote because ffmpeg requires
+        out_file = '"' + out_filename + ext + '"' # add back surrounding quotes because ffmpeg requires
 
-    # open the metadata file, clearing out contents if necessary
-    try:
-        if (do_full == True):
-            cmd = ffcmd + ' -i ' + src_file + ' -f ffmetadata ' + meta_file
-            os.system(cmd)
-            with open(meta_file_nq, 'a') as f:
-                pass
+        # update the metafile name
+        if (meta_file == None): # metadata file name
+            meta_file = "ffmetadatafile.txt"
+ 
+        # use ffmpeg to create the metadata file and open for appending
+        meta_file_nq = meta_file.strip('"').strip("'") # no quote versions for open commands
+        cmd = ffcmd + ' -i ' + src_file + ' -f ffmetadata ' + meta_file
+        os.system(cmd)
+        with open(meta_file_nq, 'a') as f:
+            pass
 
-        # open the batch file, clearing out contents if necessary
-        if (split_file == True):
-            with open(batch_file_nq, 'w') as g:
-                pass
+    # open the batch file, clearing out contents if necessary
+    if (split_file == True):
 
-    except Exception as e:
-        print(f"An error occurred in creating conversion files: {e}")
-
+        # update the batch file name
+        if (batch_file == None): # batch file name
+            batch_file = "ffbatchfile.bat"
+        
+        # open the batch file for writing
+        batch_file_nq = batch_file.strip('"').strip("'")  # no quote versions for open commands
+        with open(batch_file_nq, 'w') as g:
+            pass
 
     # process each of the timecodes in the list
     i = 1
@@ -210,30 +243,24 @@ def process_chapters(chapter_data, process_input):
             title = 'TITLE=' + chapter_name
             startkey = 'START=' + str(startmsec)
             endkey = 'END=' + str(endmsec)
-            try:
-                with open(meta_file_nq, 'a') as f:
-                    f.write(emptyline + '\n')
-                    f.write(chapterkey + '\n')
-                    f.write(timedef + '\n')
-                    f.write(startkey + '\n')
-                    f.write(endkey + '\n')
-                    f.write(title + '\n')
-            except Exception as e:
-                print(f"An error occurred in reading conversion metadata file file: {e}")
-
+            with open(meta_file_nq, 'a') as f:
+                f.write(emptyline + '\n')
+                f.write(chapterkey + '\n')
+                f.write(timedef + '\n')
+                f.write(startkey + '\n')
+                f.write(endkey + '\n')
+                f.write(title + '\n')
+ 
         # add to batch file for split
         if (split_file == True):
             # make ffmpeg command where the chapter name and the source directory are used to create a filename for output
-            chapter_name = chapter_name.translate({ord(i): None for i in '<>:/\\|?'}) # remove special windows file characters
+            chapter_name = chapter_name.translate({ord(j): None for j in '<>:/\\|?'}) # remove special windows file characters
             ffmpegname = '"' + src_dir_nq + '\\' + str(i).zfill(2) + ' ' + chapter_name.strip('"') + ext + '"'
             ffmpegcmd = ffcmd + ' -ss ' + str(startsec) + ' -to ' + str(endsec) + ' -i ' + src_file + ffopt + ffmpegname
 
             # append command to the batch flie
-            try:
-                with open(batch_file_nq, 'a') as g:
-                    g.write(ffmpegcmd + '\n')
-            except Exception as e:
-                print(f"An error occurred in reading conversion batch file: {e}")
+            with open(batch_file_nq, 'a') as g:
+                g.write(ffmpegcmd + '\n')
 
         # increment
         i = i + 1
@@ -248,19 +275,16 @@ def process_chapters(chapter_data, process_input):
            typeopt = '-c copy '
         cmd = 'ffmpeg -i ' + src_file + baseopt + typeopt + out_file
         os.system(cmd)
-        
+        if (keep_flag != True):
+            cmd = 'del ' + meta_file
+            os.system(cmd)
+
     # do splitting and cleanup batch file
     if (split_file == True):
         os.system(batch_file)
         if (keep_flag != True):
             cmd = 'del ' + batch_file
             os.system(cmd)
-
-   # cleanup chapter metadata file
-    if (do_full == True and keep_flag != True):
-        cmd = 'del ' + meta_file
-        os.system(cmd)
-
 
 
 
@@ -282,12 +306,9 @@ except Exception as e:
     print(f"An error occurred: {e}")          
 
 
-# parse the control file
-process_input = parse_control_file(procinput)
+# process the video/audio file chapter information
+process_chapters(procinput)
 
-#print(process_input)
 
-# process the chapter information
-process_chapters(process_input)
 
 ######################################################################################3333
